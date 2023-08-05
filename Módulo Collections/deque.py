63c89220baa5c5d0245527645939dc6263f8e9e3
eujotave <joaovmb15@gmail.com>
2023-08-05 03:17:34 -0300

    Adicionando deque e Corrigindo namedtuple

---
+++ Módulo Collections/deque.py
@@ -0,0 +1,64 @@
+# Importação
+
+from collections import deque
+
+"""
+O módulo deque se trata de uma lista de alta perfomance. Entretanto, age um pouco diferente de uma lista como nós
+conhecemos. No caso do deque, ao invés de aceitar vários elementos e formar uma lista, ele só aceita 1 único elemento
+e divide ele em vários outros elementos, como se fosse utilizado um .split
+
+Exemplo:
+
+fruta = deque("Banana")
+
+print(fruta) -> deque(['B', 'a', 'n', 'a', 'n', 'a'])
+
+Em listas, nós podemos utilizar os comandos:
+
+.append -> Adiciona um elemento no fim da lista
+.pop -> Remove um elemento do fim da lista
+"""
+
+# Exemplo .append
+
+lista = [1, 2, 3, 4, 4, 5]
+
+lista.append(9)
+print(lista)
+
+# Exemplo .pop
+
+lista = [1, 2, 3, 4, 4, 5]
+
+lista.pop()
+print(lista)
+
+"""
+Se percebemos, nós só conseguimos adicionar e remover elementos ao final da lista, o que pode acabar impossibilitando 
+casos que devemos adicionar ou remover valores do inicio.
+
+Nestes casos, podemos usar o deque, já que por ser uma "lista de alta perfomance", conseguimos usar os comandos:
+
+.appendleft
+.popleft 
+"""
+
+# Utilizando o deque e imprimindo os resultados
+
+# Exemplo .appendleft
+
+deq = deque("Goiaba")
+
+deq.appendleft("A")
+
+print(deq)  # Perceba que o 'A' foi adicionado ao inicio e não ao fim como normalmente ocorre
+
+# Exemplo .popleft
+
+deq = deque("Goiaba")
+
+deq.popleft()
+
+print(deq)  # Perceba que o primeiro elemento foi removido ao invés do ultimo como normalmente ocorre
+
+
--- Módulo Collections/namedtuple.py
+++ Módulo Collections/namedtuple.py
@@ -3,8 +3,11 @@
 from collections import namedtuple
 
 """
-O namedtuple se trata de uma tupla diferente, onde é possível passar um nome e parâmetros para uma tupla. Fazendo com
-que possamos injetar dados na mesma.  
+O namedtuple (Tupla Nomeada), se trata de uma tupla diferente, onde é possível passar um nome e parâmetros para uma 
+tupla (Como uma espécie de dicionário). 
+
+OBS: Diferentemente dos dicionários, as tuplas e tuplas nomeadas são indexadas, permitindo assim o acesso de cada
+elemento por meio do índice
 """
 
 # Utilizando o namedtuple
@@ -23,9 +26,9 @@ compras = namedtuple("comidas", "produto, marca, quantidade")
 
 compras = namedtuple("comidas", ["produto", "marca", "quantidade"])
 
-# Utilizando/Injetando Dados
+# Utilizando o namedtuple para atribuir valores
 
-molho = compras(produto="Molho de Tomate", marca="Quero", quantidade="3")
+molho = compras(produto="Molho de Tomate", marca="Quero", quantidade=3)
 
 # Imprimindo/Acessando Dados
 
@@ -44,3 +47,18 @@ print(molho[2])  # Quantidade
 print(molho.produto)  # Produto
 print(molho.marca)  # Marca
 print(molho.quantidade)  # Quantidade
+
+"""
+Com isso, nos podemos utilizar O MODELO (nome e parâmetros) daquela tupla em outras diversas vezes, como nesse caso 
+para adicionar novos elementos nas compras, onde também podemos acessar cada parâmetro passado. 
+"""
+
+frango = compras(produto="Peito de Frango", marca="Sadia", quantidade=2)
+milho = compras(produto="Milho Enlatado", marca="Quero", quantidade=3)
+
+print(frango)
+print(milho)
+
+print(frango.marca)
+print(milho.quantidade)
+
